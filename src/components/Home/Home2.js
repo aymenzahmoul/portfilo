a61d3cbde3462f043f7d7b257a05d7ca9480559a
aymenzahmoul <aymen.zahmoul@otospex.com>
2024-09-16 14:38:31 +0100

    version 1

---
+++ src/components/Home/Home2.js
@@ -0,0 +1,106 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/image.png";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+  AiOutlineMail,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }} data-aos="fade-right">
+              <span className="primary-header"> ABOUT </span> ME
+            </h1>
+            <p className="home-about-body" data-aos="fade-up">
+              <br />
+              I am <i className="primary-header">Aymen Zahmoul</i>, a passionate Full Stack Web and Mobile Developer with expertise in building dynamic, user-friendly applications.
+              <br />
+              <br />With experience across both front-end and back-end technologies, I specialize in frameworks like <i className="primary-header">React.js, Vue.js, Laravel,</i> and <i className="primary-header">Node.js</i>.
+              <br />
+              <br />My work spans from developing complete ERP platforms to creating mobile applications for efficient point-of-sale management.
+              <br />
+              <br />I thrive in fast-paced environments where I can continuously learn and apply my skills in <i className="primary-header">React Native, PostgreSQL, MongoDB,</i> and more.
+              <br />
+              <br />My goal is to deliver innovative solutions that solve real-world problems while offering seamless user experiences.
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img
+                data-aos="fade-left"
+                src={myImg}
+                className="img-fluid"
+                alt="avatar"
+              />
+            </Tilt>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1 data-aos="fade-right">
+              <span className="primary-header">CONNECT </span> WITH ME
+            </h1>
+            <p data-aos="fade-left">Feel free to connect with me</p>
+            <ul className="home-about-social-links" data-aos="fade-up">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/aymenzahmoul"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                  aria-label="github"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+             
+              <li className="social-icons">
+                <a
+                  href="mailto:zahmoulaymen7@gmail.com"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                  aria-label="email"
+                >
+                  <AiOutlineMail />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/aymen-zahmoul/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                  aria-label="linkedin"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              {/* <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/dead.programmer/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                  aria-label="instagram"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li> */}
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ScrollToTop from "../ScrollToTop/ScrollToTop";
+import ProjectCard from "./ProjectCards";
+
+import PrivateChat from "../../Assets/Projects/resto.png";
+import Portfolio from "../../Assets/Projects/Portfolio.png";
+import Ecart from "../../Assets/Projects/pos.png";
+import ExpenseTracker from "../../Assets/Projects/djerba.png";
+
+import "./project.css";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="Fluorescent-Blue">Works </strong>
+        </h1>
+        <p>Here are a few projects I've worked on recently.</p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={6} lg={4} className="project-card">
+            <ProjectCard
+              imgPath={Ecart}
+              title=" Development and maintenance of a complete ERP platform"
+              description="I was responsible for the development and maintenance of a comprehensive ERP platform, which included modules for inventory management, order processing, invoicing, and customer relationship management. Additionally, I developed a dedicated mobile application specifically for point-of-sale (POS) management, allowing for fast and efficient processing of in-store transactions. This project aimed to streamline business operations and improve overall productivity for the client."
+              // ghLink="https://github.com/rahuljha4171/E-cart"
+              demoLink="https://partfly.io/"
+            />
+          </Col>
+
+      
+          <Col md={6} lg={4} className="project-card">
+            <ProjectCard
+              imgPath={PrivateChat}
+              title="developed and maintained an online ordering platform for restaurants"
+              description="I developed and maintained an online ordering platform for restaurants. The platform included the integration of a secure payment system and a real-time notification module, ensuring smooth and safe transactions for users. I also implemented real-time communication features and developed content management tools tailored to restaurant needs, enabling efficient management of their offerings and services."
+              ghLink="#"
+              demoLink="https://localtestdemo.eatorder.fr/"
+            />
+          </Col>
+
+          <Col md={6} lg={4} className="project-card">
+            <ProjectCard
+              imgPath={ExpenseTracker}
+              title="developed and maintained an online ordering platform for restaurants"
+              description="This application aims to create an online platform that helps restaurants sell their dishes and manage their operations efficiently. It allows restaurants to manage their accounts, menus (create, modify, delete dishes), orders (receive, manage, deliver), and online posts. Customers can also manage their accounts, browse restaurants and their posts, and purchase food online. "
+                // ghLink="https://github.com/rahuljha4171/Expense-Tracker"
+                // demoLink="https://expense-tracker.rahuljha4171.vercel.app/"
+            />
+          </Col>
+
+          <Col md={6} lg={4} className="project-card">
+            <ProjectCard
+              imgPath={Portfolio}
+              title="Portfolio Website"
+              description="My personal Portfolio Website build with React and Bootstrap. It is fully responsive website which supports both dark and light mode."
+              ghLink="https://github.com/rahuljha4171/Portfolio-Website"
+              demoLink="https://rahuljha.info/"
+            />
+          </Col>
+        </Row>
+      </Container>
+      <ScrollToTop />
+    </Container>
+  );
+}
+
+export default Projects;
